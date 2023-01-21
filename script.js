33ee0cad72b0de6149ba9632fcc90ae433cbd474
MuhammadShameel <123203015+MuhammadShameel@users.noreply.github.com>
2023-01-21 03:15:10 -0800

    javascript practice

---
+++ script.js
@@ -0,0 +1,36 @@
+// 1. Create An Array Of Students.
+let studentsOne = [
+  { name: "Hammad", age: 17 },
+  { name: "Abdul Rafay", age: 11 },
+  { name: "Huzaifa", age: 8 },
+  { name: "Laraib", age: 16 },
+];
+
+let teachersOne = [
+  { name: "Sir Bilal", age: 27, salary: 200 },
+  { name: "Sir Areeb", age: 21 },
+  { name: "Sir Nabeel", age: 28 },
+  { name: "Sir Habeeb", age: 26 },
+];
+
+// 2.Create a function named renderTable.
+let rederTable = function (items) {
+  // 3. Create table dynamically .
+  let container = document.querySelector("#table");
+  let table = document.createElement("table");
+  for (let key in items) {
+    let row = document.createElement("tr");
+    let obj = items[key];
+    for (let objKey in obj) {
+      let cell = document.createElement("td");
+      let textNode = document.createTextNode(obj[objKey]);
+      cell.appendChild(textNode);
+      row.appendChild(cell);
+    }
+    table.appendChild(row);
+  }
+  container.appendChild(table);
+};
+
+// 4. calling function.
+rederTable(teachersOne);
