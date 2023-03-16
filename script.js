4c14c209c864c0b37f49b38cae6eccb62047b237
Muhammad Shameel <mshameelkz@gmail.com>
2023-03-16 07:36:43 -0700

    Adding New Row

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
-// 1. Create An Array Of Students.
 let studentsOne = [
-  { Name: "Hammad", Age: 17, Class: "10th" },
-  { Name: "Abdul Rafay", Age: 11, Class: "3rd" },
-  { Name: "Huzaifa", Age: 8, Class: "2nd" },
-  { Name: "Laraib", Age: 16, Class: "9th" },
+  { name: "Hammad", age: 17, class: "10th" },
+  { name: "Abdul Rafay", age: 11, class: "3rd" },
+  { name: "Huzaifa", age: 8, class: "2nd" },
+  { name: "Laraib", age: 16, class: "9th" },
 ];
 
 let teachersOne = [
-  { Name: "Sir Bilal", Age: 27, Subjects: "English" },
-  { Name: "Sir Areeb", Age: 21, Subjects: "Chemistry" },
-  { Name: "Sir Nabeel", Age: 28, Subjects: "Physics" },
-  { Name: "Sir Habeeb", Age: 26, Subjects: "Urdu" },
+  { name: "Sir Bilal", age: 27, subjects: "English" },
+  { name: "Sir Areeb", age: 21, subjects: "Chemistry" },
+  { name: "Sir Nabeel", age: 28, subjects: "Physics" },
+  { name: "Sir Habeeb", age: 26, subjects: "Urdu" },
 ];
 
-// 2.Create a function named renderTable.
-let rederTable = function (items) {
-  // 3. Create table dynamically .
-  let container = document.querySelector("#table");
+let renderTable = function (items, elementId) {
+  let container = document.querySelector(elementId);
   let table = document.createElement("table");
+  table.className = "table table-active";
+  let thead = document.createElement("thead");
+  thead.className = "table-dark";
+  let tbody = document.createElement("tbody");
+  let tfoot = document.createElement("tfoot");
 
   items.forEach((item, index) => {
     if (index === 0) {
       let row = document.createElement("tr");
       for (let itemKey in item) {
         let cell = document.createElement("td");
-        let textNode = document.createTextNode(itemKey);
-        cell.appendChild(textNode);
+        cell.innerText = itemKey.charAt(0).toUpperCase() + itemKey.slice(1);
         row.appendChild(cell);
         table.appendChild(row);
       }
+      let actionHeader = document.createElement("td");
+      actionHeader.innerText = "Action";
+      row.appendChild(actionHeader);
+      thead.appendChild(row);
     }
     let rowEl = document.createElement("tr");
     for (let itemKey in item) {
@@ -37,11 +42,112 @@ let rederTable = function (items) {
       cellEl.appendChild(textNode);
       rowEl.appendChild(cellEl);
     }
-    table.appendChild(rowEl);
+
+    let actionCell = document.createElement("td");
+    let editButton = document.createElement("button");
+    editButton.className = "btn btn-outline-primary me-md-3";
+    editButton.innerText = "Edit";
+    let saveButton = document.createElement("button");
+    saveButton.className = "btn btn-outline-success me-md-3 ";
+    saveButton.innerText = "Save";
+    saveButton.style.display = "none";
+    actionCell.appendChild(editButton);
+    actionCell.appendChild(saveButton);
+
+    let deleteButton = document.createElement("button");
+    deleteButton.className = "btn btn-outline-danger ";
+    deleteButton.innerText = "Delete";
+    actionCell.appendChild(deleteButton);
+    rowEl.appendChild(actionCell);
+    deleteButton.addEventListener("click", function () {
+      items.splice(index, 1);
+      container.removeChild(table);
+      renderTable(items, elementId);
+    });
+
+    editButton.addEventListener("click", function () {
+      Array.from(rowEl.children).forEach((cell, i) => {
+        if (i < rowEl.children.length - 1) {
+          let input = document.createElement("input");
+          input.type = "text";
+          input.style.width = "83px";
+
+          input.value = cell.innerText;
+          cell.innerText = "";
+          cell.appendChild(input);
+        }
+      });
+      editButton.style.display = "none";
+      saveButton.style.display = "inline";
+    });
+
+    saveButton.addEventListener("click", function () {
+      Array.from(rowEl.children).forEach((cell, i) => {
+        if (i < rowEl.children.length - 1) {
+          let input = cell.firstChild;
+          cell.innerText = input.value;
+        }
+      });
+      saveButton.style.display = "none";
+      editButton.style.display = "inline";
+    });
+
+    tbody.appendChild(rowEl);
   });
 
+  let newRow = document.createElement("tr");
+  let numColumns = Object.keys(items[0]).length;
+
+  let actionCell = document.createElement("td");
+  actionCell.setAttribute("colspan", numColumns + 1);
+  let addRowButton = document.createElement("button");
+  addRowButton.className = " w-100 glow-on-hover";
+  addRowButton.innerText = "+";
+  addRowButton.style.fontSize = "20px";
+  addRowButton.style.fontWeight = "bold";
+
+  actionCell.appendChild(addRowButton);
+  newRow.appendChild(actionCell);
+  tfoot.appendChild(newRow);
+
+  addRowButton.addEventListener("click", function () {
+    let newRow = document.createElement("tr");
+    for (let i = 0; i < numColumns; i++) {
+      let cellEl = document.createElement("td");
+      let input = document.createElement("input");
+      input.type = "text";
+      input.style.width = "83px";
+      cellEl.appendChild(input);
+      newRow.appendChild(cellEl);
+    }
+    let actionCell = document.createElement("td");
+    let saveButton = document.createElement("button");
+    saveButton.className = "btn btn-outline-success me-md-3 w-100";
+    saveButton.innerText = "Save";
+    actionCell.appendChild(saveButton);
+    newRow.appendChild(actionCell);
+    tbody.insertBefore(newRow, newRow.previousSibling);
+
+    saveButton.addEventListener("click", function () {
+      let rowValues = {};
+      Array.from(newRow.children).forEach((cell, i) => {
+        if (i < newRow.children.length - 1) {
+          let input = cell.firstChild;
+          rowValues[Object.keys(items[0])[i]] = input.value;
+        }
+      });
+
+      items.push(rowValues);
+      container.removeChild(table);
+      renderTable(items, elementId);
+    });
+  });
+
+  table.appendChild(thead);
+  table.appendChild(tbody);
+  table.appendChild(tfoot);
   container.appendChild(table);
 };
 
-// 4. calling function.
-rederTable(teachersOne);
+renderTable(teachersOne, "#table");
+renderTable(studentsOne, "#tableTwo");
