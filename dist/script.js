68d355270ba12f665a1c1702203b04eab6dd81e8
MuhammadShameel <mshameelkz@gmail.com>
2023-05-06 22:37:46 -0700

    src and dit folder created

--- dist/script.js
+++ dist/script.js
@@ -1,90 +1,71 @@
-interface Student {
-  name: string;
-  age: number;
-  class: string;
-  [key: string]: any;
-}
-
-interface Teacher {
-  name: string;
-  age: number;
-  subjects: string;
-  [key: string]: any;
-}
-
-const students: Student[] = [
+var Students = [
   { name: "Hammad", age: 17, class: "10th" },
   { name: "Abdul Rafay", age: 11, class: "3rd" },
   { name: "Huzaifa", age: 8, class: "2nd" },
   { name: "Laraib", age: 16, class: "9th" },
 ];
-
-const teachers: Teacher[] = [
+var Teachers = [
   { name: "Sir Bilal", age: 27, subjects: "English" },
   { name: "Sir Areeb", age: 21, subjects: "Chemistry" },
   { name: "Sir Nabeel", age: 28, subjects: "Physics" },
   { name: "Sir Habeeb", age: 26, subjects: "Urdu" },
 ];
 
-function renderTable(items: any[], elementId: string) {
-  const container = document.getElementById(elementId) as HTMLElement;
+function renderTable(items, elementId) {
+  var container = document.getElementById(elementId);
   container.className = "table table-striped";
-  const table = document.createElement("table");
-  const tableHead = document.createElement("thead");
-  const headerRow = document.createElement("tr");
-  Object.keys(items[0]).forEach((key) => {
-    const headerCell = document.createElement("th");
+  var table = document.createElement("table");
+  var tableHead = document.createElement("thead");
+  var headerRow = document.createElement("tr");
+  Object.keys(items[0]).forEach(function (key) {
+    var headerCell = document.createElement("th");
     headerCell.className = "text-center p-2";
     headerCell.textContent = key.charAt(0).toUpperCase() + key.slice(1);
     headerRow.appendChild(headerCell);
   });
   tableHead.appendChild(headerRow);
   table.appendChild(tableHead);
-
-  const tableBody = document.createElement("tbody");
-  items.forEach((item: any, index: number) => {
-    const row = document.createElement("tr");
+  var tableBody = document.createElement("tbody");
+  items.forEach(function (item, index) {
+    var row = document.createElement("tr");
     row.className = "text-center";
-    Object.keys(item).forEach((key) => {
-      const cell = document.createElement("td");
+    Object.keys(item).forEach(function (key) {
+      var cell = document.createElement("td");
       cell.className = "p-2";
       cell.textContent = item[key];
       row.appendChild(cell);
     });
-
-    const deleteCell = document.createElement("td");
-    const deleteButton = document.createElement("button");
+    var deleteCell = document.createElement("td");
+    var deleteButton = document.createElement("button");
     deleteButton.className = "btn btn-secondary btn-sm";
     deleteButton.textContent = "Delete";
-    deleteButton.addEventListener("click", () => {
+    deleteButton.addEventListener("click", function () {
       items.splice(index, 1);
       container.removeChild(table);
       renderTable(items, elementId);
     });
-
-    const editCell = document.createElement("td");
-    const editButton = document.createElement("button");
+    var editCell = document.createElement("td");
+    var editButton = document.createElement("button");
     editButton.className = "btn btn-secondary btn-sm";
     editButton.textContent = "Edit";
-    editButton.addEventListener("click", () => {
-      const editRow = document.createElement("tr");
+    editButton.addEventListener("click", function () {
+      var editRow = document.createElement("tr");
       editRow.className = "text-center";
-      Object.keys(item).forEach((key) => {
-        const editCell = document.createElement("td");
+      Object.keys(item).forEach(function (key) {
+        var editCell = document.createElement("td");
         editCell.className = "p-2";
-        const editInput = document.createElement("input");
+        var editInput = document.createElement("input");
         editInput.value = item[key];
         editCell.appendChild(editInput);
         editRow.appendChild(editCell);
       });
-
-      const saveCell = document.createElement("td");
-      const saveButton = document.createElement("button");
+      var saveCell = document.createElement("td");
+      var saveButton = document.createElement("button");
       saveButton.className = "btn btn-secondary btn-sm";
       saveButton.textContent = "Save";
-      saveButton.addEventListener("click", () => {
-        const editInputs = editRow.querySelectorAll("input");
-        editInputs.forEach((editInput, i) => {
+      saveButton.addEventListener("click", function () {
+        var editInputs = editRow.querySelectorAll("input");
+        editInputs.forEach(function (editInput, i) {
           item[Object.keys(item)[i]] = editInput.value;
         });
         container.removeChild(table);
@@ -92,25 +73,19 @@ function renderTable(items: any[], elementId: string) {
       });
       saveCell.appendChild(saveButton);
       editRow.appendChild(saveCell);
-
       if (row.parentNode) {
         row.parentNode.insertBefore(editRow, row.nextSibling);
         row.style.display = "none";
       }
     });
-
     deleteCell.appendChild(deleteButton);
     row.appendChild(deleteCell);
-
     editCell.appendChild(editButton);
     row.appendChild(editCell);
-
     tableBody.appendChild(row);
   });
-
   table.appendChild(tableBody);
   container.appendChild(table);
 }
-
-renderTable(students, "students-table");
-renderTable(teachers, "teachers-table");
+renderTable(Students, "students-table");
+renderTable(Teachers, "teachers-table");
--- src/script.ts
+++ src/script.ts
@@ -1,71 +1,90 @@
-var Students = [
+interface Student {
+  name: string;
+  age: number;
+  class: string;
+  [key: string]: any;
+}
+
+interface Teacher {
+  name: string;
+  age: number;
+  subjects: string;
+  [key: string]: any;
+}
+
+const students: Student[] = [
   { name: "Hammad", age: 17, class: "10th" },
   { name: "Abdul Rafay", age: 11, class: "3rd" },
   { name: "Huzaifa", age: 8, class: "2nd" },
   { name: "Laraib", age: 16, class: "9th" },
 ];
-var Teachers = [
+
+const teachers: Teacher[] = [
   { name: "Sir Bilal", age: 27, subjects: "English" },
   { name: "Sir Areeb", age: 21, subjects: "Chemistry" },
   { name: "Sir Nabeel", age: 28, subjects: "Physics" },
   { name: "Sir Habeeb", age: 26, subjects: "Urdu" },
 ];
 
-function renderTable(items, elementId) {
-  var container = document.getElementById(elementId);
+function renderTable(items: any[], elementId: string) {
+  const container = document.getElementById(elementId) as HTMLElement;
   container.className = "table table-striped";
-  var table = document.createElement("table");
-  var tableHead = document.createElement("thead");
-  var headerRow = document.createElement("tr");
-  Object.keys(items[0]).forEach(function (key) {
-    var headerCell = document.createElement("th");
+  const table = document.createElement("table");
+  const tableHead = document.createElement("thead");
+  const headerRow = document.createElement("tr");
+  Object.keys(items[0]).forEach((key) => {
+    const headerCell = document.createElement("th");
     headerCell.className = "text-center p-2";
     headerCell.textContent = key.charAt(0).toUpperCase() + key.slice(1);
     headerRow.appendChild(headerCell);
   });
   tableHead.appendChild(headerRow);
   table.appendChild(tableHead);
-  var tableBody = document.createElement("tbody");
-  items.forEach(function (item, index) {
-    var row = document.createElement("tr");
+
+  const tableBody = document.createElement("tbody");
+  items.forEach((item: any, index: number) => {
+    const row = document.createElement("tr");
     row.className = "text-center";
-    Object.keys(item).forEach(function (key) {
-      var cell = document.createElement("td");
+    Object.keys(item).forEach((key) => {
+      const cell = document.createElement("td");
       cell.className = "p-2";
       cell.textContent = item[key];
       row.appendChild(cell);
     });
-    var deleteCell = document.createElement("td");
-    var deleteButton = document.createElement("button");
+
+    const deleteCell = document.createElement("td");
+    const deleteButton = document.createElement("button");
     deleteButton.className = "btn btn-secondary btn-sm";
     deleteButton.textContent = "Delete";
-    deleteButton.addEventListener("click", function () {
+    deleteButton.addEventListener("click", () => {
       items.splice(index, 1);
       container.removeChild(table);
       renderTable(items, elementId);
     });
-    var editCell = document.createElement("td");
-    var editButton = document.createElement("button");
+
+    const editCell = document.createElement("td");
+    const editButton = document.createElement("button");
     editButton.className = "btn btn-secondary btn-sm";
     editButton.textContent = "Edit";
-    editButton.addEventListener("click", function () {
-      var editRow = document.createElement("tr");
+    editButton.addEventListener("click", () => {
+      const editRow = document.createElement("tr");
       editRow.className = "text-center";
-      Object.keys(item).forEach(function (key) {
-        var editCell = document.createElement("td");
+      Object.keys(item).forEach((key) => {
+        const editCell = document.createElement("td");
         editCell.className = "p-2";
-        var editInput = document.createElement("input");
+        const editInput = document.createElement("input");
         editInput.value = item[key];
         editCell.appendChild(editInput);
         editRow.appendChild(editCell);
       });
-      var saveCell = document.createElement("td");
-      var saveButton = document.createElement("button");
+
+      const saveCell = document.createElement("td");
+      const saveButton = document.createElement("button");
       saveButton.className = "btn btn-secondary btn-sm";
       saveButton.textContent = "Save";
-      saveButton.addEventListener("click", function () {
-        var editInputs = editRow.querySelectorAll("input");
-        editInputs.forEach(function (editInput, i) {
+      saveButton.addEventListener("click", () => {
+        const editInputs = editRow.querySelectorAll("input");
+        editInputs.forEach((editInput, i) => {
           item[Object.keys(item)[i]] = editInput.value;
         });
         container.removeChild(table);
@@ -73,30 +92,33 @@ function renderTable(items, elementId) {
       });
       saveCell.appendChild(saveButton);
       editRow.appendChild(saveCell);
+
       if (row.parentNode) {
         row.parentNode.insertBefore(editRow, row.nextSibling);
         row.style.display = "none";
       }
     });
+
     deleteCell.appendChild(deleteButton);
     row.appendChild(deleteCell);
+
     editCell.appendChild(editButton);
     row.appendChild(editCell);
+
     tableBody.appendChild(row);
   });
+
   table.appendChild(tableBody);
   container.appendChild(table);
 }
-renderTable(Students, "students-table");
-renderTable(Teachers, "teachers-table");
-// function getCategories(callback) {
-//   fetch("https://apis.explorebtk.com/api/v1/categories")
-//     .then((response) => response.json())
-//     .then((categories) => {
-//       callback(categories);
-//     })
-//     .catch((error) => console.error(error));
-// }
-// getCategories((categories) => {
-//   renderTable(categories, "#tableThree");
-// });
+
+renderTable(students, "students-table");
+renderTable(teachers, "teachers-table");
+
+function invokeAnything(callback: unknown) {
+  if (typeof callback === "function") {
+    callback();
+  }
+}
+
+invokeAnything(1);
